d4890bf895681b5a319ba0d5513816e15d334245
malun22 <malun22@web.de>
2025-03-07 18:18:10 +0100

    Init

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+const redirectsPath = path.join(__dirname, "redirects.json");
+const urlMapping: Record<string, string> = JSON.parse(
+  fs.readFileSync(redirectsPath, "utf-8")
+);
+
+app.get("/Trackmania/:fileName", (req, res) => {
+  const fileName = req.params.fileName;
+
+  if (urlMapping[fileName]) {
+    res.redirect(301, urlMapping[fileName]); // Permanent redirect
+  } else {
+    res.status(404).send("File not found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
